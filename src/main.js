0f7e5229428a814766a1c620e517e6a88b50143d
shikha-jhaa <142150268+shikha-jhaa@users.noreply.github.com>
2024-03-01 16:49:32 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Create a JSX element to render
+const rootElement = (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+// Render the JSX element using ReactDOM.createRoot
+ReactDOM.createRoot(document.getElementById('root')).render(rootElement);
+
 // Check if any element in the document has focus
 const focusedElement = document.activeElement;
 
